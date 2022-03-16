dc8f49f2d9347609652e08965671a1f4e1f0bea9
CorneliaBz <bergentz2@hotmail.com>
2022-03-16 11:52:12 +0100

    flera bilder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-const img = document.createElement('img');
-document.body.append(img);
-//Funkar inte
-// img.src = './img.drake.jpg';
+// const img = document.createElement('img');
+// document.body.append(img);
+// //Funkar inte
+// // img.src = './img.drake.jpg';
 
-const imgURL = new URL ( './img/drake.jpg', import.meta.url);
-img.src = imgURL.toString(); //eller .href
-console.log(imgURL);
+// const imgURL = new URL ( './img/drake.jpg', import.meta.url);
+// img.src = imgURL.toString(); //eller .href
+// console.log(imgURL);
+
+const imgNames = ['drake', 'pingvin', 'sal'];
+for(const el of imgNames){
+    const img = document.createElement('img');
+    document.body.append(img);
+    const imgURL = new URL ( `./img/${el}.jpg`, import.meta.url);
+    img.src = imgURL.toString();
+}
